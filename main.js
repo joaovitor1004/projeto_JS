6f25fc2f7cd408fdd3e31b4d0ec38916f70241b7
joaovitor1004 <108817899+joaovitor1004@users.noreply.github.com>
2024-03-18 10:45:45 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -35,7 +35,7 @@ function calculaTempo(tempoObjetivo){
     minutos %= 60;
     horas %= 24;
     if(tempoFinal > 0){
-        return [dias, horas, min, seg]
+        return [dias,horas,minutos,segundos]
     } else {
         return [0,0,0,0];
     }
@@ -43,12 +43,15 @@ function calculaTempo(tempoObjetivo){
 }
 
 function atualizaCronometro(){
-    document.getElementById("dias0").textContent = calculaTempo(tempos[0])[0];
-    document.getElementById("horas0").textContent = calculaTempo(tempos[0])[1];
-    document.getElementById("min0").textContent = calculaTempo(tempos[0])[2];
-    document.getElementById("seg0").textContent = calculaTempo(tempos[0])[3];
     for (let i=0; i<contadores.length; i++){
-        contadores[i].textContent = calculaTempo(tempos[i]);
+        document.getElementById("dias"+i).textContent = calculaTempo(tempos[i])[0];
+        console.log("dias"+i)
+        document.getElementById("horas"+i).textContent = calculaTempo(tempos[i])[1];
+        console.log("horas"+i)
+        document.getElementById("min"+i).textContent = calculaTempo(tempos[i])[2];
+        console.log("min"+i)
+        document.getElementById("seg"+i).textContent = calculaTempo(tempos[i])[3];
+        console.log("seg"+i);
     }
 }
 
