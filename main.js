00ee6ecca35b5f54282897a3c93b86b4a5b5022d
joaovitor1004 <108817899+joaovitor1004@users.noreply.github.com>
2024-03-27 07:51:48 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -45,13 +45,9 @@ function calculaTempo(tempoObjetivo){
 function atualizaCronometro(){
     for (let i=0; i<contadores.length; i++){
         document.getElementById("dias"+i).textContent = calculaTempo(tempos[i])[0];
-        console.log("dias"+i)
         document.getElementById("horas"+i).textContent = calculaTempo(tempos[i])[1];
-        console.log("horas"+i)
         document.getElementById("min"+i).textContent = calculaTempo(tempos[i])[2];
-        console.log("min"+i)
         document.getElementById("seg"+i).textContent = calculaTempo(tempos[i])[3];
-        console.log("seg"+i);
     }
 }
 
